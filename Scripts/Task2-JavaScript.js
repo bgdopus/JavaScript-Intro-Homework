2ba55634cb15186d7f273e13b533241530ee7f3c
Daniel Georgiev <bgdopus@gmail.com>
2015-11-30 22:13:12 +0200

    homework

---
+++ Scripts/Task2-JavaScript.js
@@ -0,0 +1,8 @@
+﻿
+function circleArea() {
+    var radius = prompt("Please enter the radius" );
+    if (radius != null) {
+        document.getElementById("demo").innerHTML =
+        "Radius ="+ radius + " Area =" + Math.PI * Math.pow(radius, 2);
+    }
+}
---
+++ Scripts/dec2hex-script.js
@@ -0,0 +1,10 @@
+﻿function calculate() {
+    var number = prompt("Enter a decimal number");
+    if (number < 0) {
+        number = 0xFFFFFFFF + number + 1;
+    }
+
+    
+    var result = (+number).toString(16).toUpperCase();
+    alert(result);
+}
